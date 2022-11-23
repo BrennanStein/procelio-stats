554ffc1e33ffb7e31649c48f9c451bb8ac0992ac
Brennan Stein <brennan.stein@outlook.com>
2022-11-22 17:56:34 -0800

    update stats

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ scp -i ~/.ssh/digitalocean ./statfile.stat root@files.procelio.com:files/files/p
 ssh -i ~/.ssh/digitalocean root@files.procelio.com "tmux kill-session -t files; tmux new-session -d -c files -s files './file-server >> log 2>&1'"
 
 sleep 10
-ssh -i ~/.ssh/acct_digitalocean accounts@files.procelio.com "tmux kill-session -t accounts; ./launch_all.sh"
+ssh -i ~/.ssh/acct_digitalocean root@accounts.procelio.com "tmux kill-session -t accounts; ./launch_all.sh"
 
