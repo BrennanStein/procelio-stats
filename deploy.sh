1d0fbcdab4cc59ce9fd5b4d6f80a8588d00bcd38
Brennan Stein <brennan.stein@outlook.com>
2023-02-22 20:29:15 -0800

    s3cmd stats too

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,5 @@ ssh -i ~/.ssh/digitalocean root@files.procelio.com "tmux kill-session -t files; 
 
 sleep 10
 ssh -i ~/.ssh/acct_digitalocean root@accounts.procelio.com "tmux kill-session -t accounts; ./launch_all.sh"
-
+s3cmd put ./statfile.stat s3://procelio/stats/prod.stat
+reloadstats
