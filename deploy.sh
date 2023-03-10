f0d2deeddf06f185023beb87efa0910aea3ce74c
Brennan Stein <brennan.stein@outlook.com>
2023-03-10 12:16:31 -0800

    update stats

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,7 @@ git merge origin/master
 ./compile.sh
 git commit -am "update stats"
 git push
-scp -i ~/.ssh/digitalocean ./statfile.stat root@files.procelio.com:files/files/prod/statfile.stat
 
-ssh -i ~/.ssh/digitalocean root@files.procelio.com "tmux kill-session -t files; tmux new-session -d -c files -s files './file-server >> log 2>&1'"
-
-sleep 10
-ssh -i ~/.ssh/acct_digitalocean root@accounts.procelio.com "tmux kill-session -t accounts; ./launch_all.sh"
 s3cmd put ./statfile.stat s3://procelio/stats/prod.stat
 curl -X POST https://releases.procelio.com:9630/admin/reload_stats -d "$RELOADKEY"
+ssh -i ~/.ssh/acct_digitalocean root@accounts.procelio.com "tmux kill-session -t accounts; ./launch_all.sh"
